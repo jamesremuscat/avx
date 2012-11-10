ef0f314da96b4f09680835ce180904e8d31c9e79
James Muscat <james@71wytham.org.uk>
2012-11-10 20:55:20 +0000

    Definition for the Inline 3808 switcher.

---
+++ src/org/muscat/staldates/aldatesx/devices/Inline3808.py
@@ -0,0 +1,13 @@
+from org.muscat.staldates.aldatesx.devices.SerialDevice import SerialDevice
+
+class Inline3808(SerialDevice):
+    '''
+    An Inline 3808 8x4 switcher.
+    '''
+
+    def __init__(self, deviceID, serialDevice):
+        super(Inline3808, self).__init__(deviceID, serialDevice)
+        self.sendCommand("[CNF290000]") # All boards to group 1. Copying what the AMX does currently though this should be the default.
+        
+    def sendInputToOutput(self, inChannel, outChannel):
+        self.sendCommand("[PT1O0" + str(outChannel) + "I0" + str(inChannel) + "]")
