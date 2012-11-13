3763278b82705355ac4be4a17eed881acb7b9601
James Muscat <jrem@corefiling.com>
2012-11-13 12:41:19 +0000

    Config for the Coriogen Eclipse scan converter/genlock (easy testing target?)

---
+++ src/org/muscat/staldates/aldatesx/devices/CoriogenEclipse.py
@@ -0,0 +1,27 @@
+'''
+Created on 13 Nov 2012
+
+@author: jrem
+'''
+from org.muscat.staldates.aldatesx.devices.SerialDevice import SerialDevice
+
+class CoriogenEclipse(SerialDevice):
+    '''
+    Coriogen Eclipse scan converter/genlock/overlay
+    '''
+
+
+    def __init__(self, serialDevice):
+        super(CoriogenEclipse, self).__init__(1, serialDevice)
+
+    def overlayOn(self):
+        self.sendCommand("Mode = 3\x13")
+        
+    def overlayOff(self):
+        self.sendCommand("Mode = 0\x13")
+        
+    def fadeOut(self):
+        self.sendCommand("Fade = 0\x13")
+        
+    def fadeIn(self):
+        self.sendCommand("Fade = 1\x13")
