da55f85460ece93c5b266b0a091c9aa39ebb2241
James Muscat <jrem@corefiling.com>
2012-11-13 15:16:41 +0000

    Change the line breaks and suddenly this works... it's a start!

--- src/org/muscat/staldates/aldatesx/devices/CoriogenEclipse.py
+++ src/org/muscat/staldates/aldatesx/devices/CoriogenEclipse.py
@@ -15,19 +15,19 @@ class CoriogenEclipse(SerialDevice):
         super(CoriogenEclipse, self).__init__(1, serialDevice)
 
     def overlayOn(self):
-        self.sendCommand("Mode = 3\x13")
+        self.sendCommand("Mode = 3\r\n")
         
     def overlayOff(self):
-        self.sendCommand("Mode = 0\x13")
+        self.sendCommand("Mode = 0\r\n")
         
     def fadeOut(self):
-        self.sendCommand("Fade = 0\x13")
+        self.sendCommand("Fade = 0\r\n")
         
     def fadeIn(self):
-        self.sendCommand("Fade = 1\x13")
+        self.sendCommand("Fade = 1\r\n")
     
     def freeze(self):
-        self.sendCommand("Freeze = On\x13")
+        self.sendCommand("Freeze = On\r\n")
     
     def unfreeze(self):
-        self.sendCommand("Freeze = Off\x13")
+        self.sendCommand("Freeze = Off\r\n")
