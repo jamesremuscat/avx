eb7e81c669730ad251ba08a7f9b9270e9a266e76
James Muscat <jrem@corefiling.com>
2012-12-24 22:09:30 +0000

    VP-703SC scan converter, can freeze and over/underscan.

---
+++ src/org/muscat/staldates/aldatesx/devices/KramerVP703.py
@@ -0,0 +1,22 @@
+from org.muscat.staldates.aldatesx.devices.SerialDevice import SerialDevice
+
+class KramerVP703(SerialDevice):
+    '''
+    Kramer VP703-SC scan converter.
+    '''
+
+
+    def __init__(self, deviceID, serialDevice):
+        super(KramerVP703, self).__init__(deviceID, serialDevice)
+        
+    def overscanOn(self):
+        return self.sendCommand("Overscan = 1\r\n")
+        
+    def overscanOff(self):
+        return self.sendCommand("Overscan = 0\r\n")
+    
+    def freeze(self):
+        return self.sendCommand("Image Freeze = 1\r\n")
+    
+    def unfreeze(self):
+        return self.sendCommand("Image Freeze = 0\r\n")
