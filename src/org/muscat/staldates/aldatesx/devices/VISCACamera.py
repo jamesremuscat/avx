6ea928323565c3198c36aa1f85bde20b8b9b5f25
James Muscat <jrem@corefiling.com>
2012-12-24 21:21:39 +0000

    Too many 0xFFs make a FFail.

--- src/org/muscat/staldates/aldatesx/devices/VISCACamera.py
+++ src/org/muscat/staldates/aldatesx/devices/VISCACamera.py
@@ -63,15 +63,15 @@ class VISCACamera(SerialDevice):
         return self.sendVISCA([0x01, 0x04, 0x38, 0x03])
     
     def brighter(self):
-        self.sendVISCA([0x01, 0x04, 0x39, 0x0D, 0xFF]) # Put camera into manual exposure mode first!
-        return self.sendVISCA([0x01, 0x04, 0x0D, 0x02, 0xFF])
+        self.sendVISCA([0x01, 0x04, 0x39, 0x0D]) # Put camera into manual exposure mode first!
+        return self.sendVISCA([0x01, 0x04, 0x0D, 0x02])
     
     def darker(self):
-        self.sendVISCA([0x01, 0x04, 0x39, 0x0D, 0xFF]) # Put camera into manual exposure mode first!
-        return self.sendVISCA([0x01, 0x04, 0x0D, 0x03, 0xFF])
+        self.sendVISCA([0x01, 0x04, 0x39, 0x0D]) # Put camera into manual exposure mode first!
+        return self.sendVISCA([0x01, 0x04, 0x0D, 0x03])
     
     def autoExposure(self):
-        return self.sendVISCA([0x01, 0x04, 0x39, 0x00, 0xFF])
+        return self.sendVISCA([0x01, 0x04, 0x39, 0x00])
         
     def storePreset(self, preset):
         if preset < 0 or preset > 5:
