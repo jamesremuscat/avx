193c65b977c9e2d54fee0545bdd97d60273db0ab
James Muscat <jrem@corefiling.com>
2012-12-27 14:06:33 +0000

    Lay out power control screen better.

--- src/org/muscat/staldates/aldatesx/widgets/SystemPowerWidget.py
+++ src/org/muscat/staldates/aldatesx/widgets/SystemPowerWidget.py
@@ -1,4 +1,5 @@
-from PySide.QtGui import QWidget, QGridLayout, QLabel
+from PySide.QtGui import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSizePolicy
+from PySide.QtCore import Qt
 from org.muscat.staldates.aldatesx.widgets.Buttons import ExpandingButton
 
 class SystemPowerWidget(QWidget):
@@ -7,12 +8,27 @@ class SystemPowerWidget(QWidget):
     def __init__(self):
         super(SystemPowerWidget, self).__init__()
 
-        layout = QGridLayout(self)
+        layout = QVBoxLayout(self)
         
         title = QLabel("System Power")
-        layout.addWidget(title, 0, 0, 1, 2)
+        title.setStyleSheet("font-size: 48px;")
+        title.setAlignment(Qt.AlignCenter)
+        layout.addWidget(title)
+        
+        buttons = QHBoxLayout()
+        
+        self.btnOff = ExpandingButton()
+        self.btnOff.setText("Off")
+        buttons.addWidget(self.btnOff)
+        
+        self.btnOn = ExpandingButton()
+        self.btnOn.setText("On")
+        buttons.addWidget(self.btnOn)
+        
+        layout.addLayout(buttons)
         
         self.b = ExpandingButton()
-        self.b.setText("Test")
-        layout.addWidget(self.b, 1,0,1,1)
+        self.b.setText("Back")
+        self.b.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        layout.addWidget(self.b)
         
