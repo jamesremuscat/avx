5251c7f1aa062276ea31ecc3700e9beb267cf6fc
James Muscat <james@71wytham.org.uk>
2013-01-03 16:33:32 +0000

    This comment might be a lie...

--- src/org/muscat/staldates/aldatesx/devices/KramerVP88.py
+++ src/org/muscat/staldates/aldatesx/devices/KramerVP88.py
@@ -8,7 +8,6 @@ from org.muscat.staldates.aldatesx.devices.SerialDevice import SerialDevice
 class KramerVP88(SerialDevice):
     '''
     A Kramer VP88 switcher, controlled by Protocol 2000 over serial.
-    Note that it's also possible to control these switchers over Ethernet and using Protocol 3000 but let's keep it simple ;)
     '''
     def __init__(self, deviceID, serialDevice, machineNumber = 1):
         super(KramerVP88, self).__init__(deviceID, serialDevice)
