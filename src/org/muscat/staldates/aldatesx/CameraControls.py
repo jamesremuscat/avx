6feab80698d1f6bf256423b442bb3c345f4db8fc
James Muscat <jamesremuscat@gmail.com>
2013-02-06 23:44:24 +0000

    Auto-focus button.

--- src/org/muscat/staldates/aldatesx/CameraControls.py
+++ src/org/muscat/staldates/aldatesx/CameraControls.py
@@ -37,6 +37,10 @@ class PlusMinusButtons(QWidget):
         layout.addWidget(self.caption, 1, 0)
         layout.addWidget(self.downButton, 2, 0)
         
+        layout.setRowStretch(0, 2)
+        layout.setRowStretch(1, 2)
+        layout.setRowStretch(2, 2)
+        
     def connectPressed(self, connection):
         self.upButton.pressed.connect(connection)
         self.downButton.pressed.connect(connection)
@@ -48,6 +52,26 @@ class PlusMinusButtons(QWidget):
     def connectClicked(self, connection):
         self.upButton.clicked.connect(connection)
         self.downButton.clicked.connect(connection)
+        
+class PlusMinusAutoButtons(PlusMinusButtons):
+    def __init__(self, caption, upBinding, downBinding, autoBinding):
+        self.autoButton = CameraButton(autoBinding)
+        self.autoButton.setText("Auto")
+        super(PlusMinusAutoButtons, self).__init__(caption, upBinding, downBinding)
+        
+    def initLayout(self):
+        layout = QGridLayout()
+        self.setLayout(layout)
+        
+        layout.addWidget(self.upButton, 0, 0)
+        layout.addWidget(self.caption, 1, 0)
+        layout.addWidget(self.autoButton, 2, 0)
+        layout.addWidget(self.downButton, 3, 0)
+        
+        layout.setRowStretch(0, 2)
+        layout.setRowStretch(1, 1)
+        layout.setRowStretch(2, 1)
+        layout.setRowStretch(3, 2)
 
 class CameraControl(QWidget):
     '''
@@ -93,9 +117,10 @@ class CameraControl(QWidget):
         zoomInOut.connectReleased(self.stopZoom)
         layout.addWidget(zoomInOut, 0, 3, 4, 1)
         
-        focus = PlusMinusButtons("Focus", CameraFocus.Far, CameraFocus.Near)
+        focus = PlusMinusAutoButtons("Focus", CameraFocus.Far, CameraFocus.Near, CameraFocus.Auto)
         focus.connectPressed(self.focus)
         focus.connectReleased(self.stopFocus)
+        focus.autoButton.clicked.connect(self.focus)
         layout.addWidget(focus, 0, 4, 4, 1)
         
         brightness = PlusMinusButtons("Brightness", True, False)
