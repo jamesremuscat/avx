68cd9d1e3050989913e05d22e490a077946f8d0e
James Muscat <jrem@corefiling.com>
2013-02-09 19:40:43 +0000

    Some useful controller-side debugging.

--- src/org/muscat/staldates/aldatesx/Controller.py
+++ src/org/muscat/staldates/aldatesx/Controller.py
@@ -24,6 +24,7 @@ class Controller(object):
     def switch(self, deviceID, inChannel, outChannel):
         '''If a device with the given ID exists, perform a video switch. If not then return -1.'''
         if self.devices.has_key(deviceID) :
+            logging.debug("Switching device " + deviceID + ": " + str(inChannel) + "=>" + str(outChannel))
             return self.devices[deviceID].sendInputToOutput(inChannel, outChannel)
         else:
             logging.warn("No device with ID " + deviceID)
