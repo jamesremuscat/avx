60e5f9b5ed6c1b6100a658447f6c5201bde11ba5
James Muscat <jamesremuscat@gmail.com>
2013-03-08 19:35:01 +0000

    Command-line option to start full-screen (and hide the mouse cursor).

--- src/AldatesX.py
+++ src/AldatesX.py
@@ -4,12 +4,14 @@ Created on 8 Nov 2012
 
 @author: james
 '''
-from PySide.QtGui import QApplication
+from PySide.QtGui import QApplication, QCursor
+from PySide.QtCore import Qt
 from org.muscat.staldates.aldatesx.Controller import Controller
 from org.muscat.staldates.aldatesx.VideoSwitcher import VideoSwitcher
 import Pyro4
 import sys
 import logging
+import argparse
 
 class AldatesX(VideoSwitcher):
     
@@ -19,6 +21,10 @@ class AldatesX(VideoSwitcher):
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
     app = QApplication(sys.argv)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fullscreen", help="Run in fullscreen mode and hide the mouse cursor", action="store_true")
+    args = parser.parse_args()
 
     try:
         stylesheet = open("AldatesX.qss", "r")
@@ -30,5 +36,9 @@ if __name__ == "__main__":
     controller = Pyro4.Proxy("PYRONAME:" + Controller.pyroName)
     
     myapp = AldatesX(controller)
-    myapp.show()
+    if args.fullscreen:
+        QApplication.setOverrideCursor(Qt.BlankCursor)
+        myapp.showFullScreen()
+    else:
+        myapp.show()
     sys.exit(app.exec_())
