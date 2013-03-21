f496084d9e9aa0bbdcdc8a7bf419a12ed2a5ad99
James Muscat <jamesremuscat@gmail.com>
2013-03-21 22:21:44 +0000

    More VISCA commands.

--- src/org/muscat/staldates/aldatesx/devices/VISCACommands.py
+++ src/org/muscat/staldates/aldatesx/devices/VISCACommands.py
@@ -3,7 +3,6 @@ Created on 21 Mar 2013
 
 @author: jrem
 '''
-
 class VISCACommand(object):
     '''
     Encapsulates a VISCA command.
@@ -18,8 +17,13 @@ class VISCACommand(object):
 
 # CAM_Zoom
 class ZoomStop(VISCACommand):
+
+    def __init__(self, cameraID = 1):
+        super(ZoomStop, self).__init__(cameraID)
+        
     def getInnerBytes(self):
         return [0x01, 0x04, 0x07, 0x00]
+        
         
 class ZoomIn(VISCACommand):
     
@@ -33,10 +37,11 @@ class ZoomIn(VISCACommand):
     def getInnerBytes(self):
         return [0x01, 0x04, 0x07, 0x20 + self.speed]
     
+    
 class ZoomOut(VISCACommand):
     
     def __init__(self, speed = 2, cameraID = 1):
-        super(ZoomIn, self).__init__(cameraID)
+        super(ZoomOut, self).__init__(cameraID)
         if 1 < speed < 8:
             self.speed = speed
         else:
@@ -46,24 +51,69 @@ class ZoomOut(VISCACommand):
         return [0x01, 0x04, 0x07, 0x30 + self.speed]
     
     
+class ZoomDirect(VISCACommand):
+    
+    def __init__(self, zoom, cameraID = 1):
+        super(ZoomDirect, self).__init__(cameraID)
+        if 0 <= zoom < 0x3FF:
+            self.zoom = zoom
+        else:
+            self.zoom = 0
+    
+    def getInnerBytes(self):
+        return [
+            0x01, 
+            0x04, 
+            0x47, 
+            (self.zoom & 0xF000) >> 12,
+            (self.zoom & 0x0F00) >> 8,
+            (self.zoom & 0x00F0) >> 4,
+            (self.zoom & 0x000F)
+            ]
+    
+    
 # CAM_Focus
 class FocusStop(VISCACommand):
+    
+    def __init__(self, cameraID = 1):
+        super(FocusStop, self).__init__(cameraID)
+        
     def getInnerBytes(self):
         return [0x01, 0x04, 0x08, 0x00]
     
+    
 class FocusFar(VISCACommand):
+    
+    def __init__(self, cameraID = 1):
+        super(FocusFar, self).__init__(cameraID)
+        
     def getInnerBytes(self):
         return [0x01, 0x04, 0x08, 0x02]
     
+    
 class FocusNear(VISCACommand):
+    
+    def __init__(self, cameraID = 1):
+        super(FocusNear, self).__init__(cameraID)
+        
     def getInnerBytes(self):
         return [0x01, 0x04, 0x08, 0x03]
     
+    
 class FocusAuto(VISCACommand):
+    
+    def __init__(self, cameraID = 1):
+        super(FocusAuto, self).__init__(cameraID)
+        
     def getInnerBytes(self):
         return [0x01, 0x04, 0x38, 0x02]
     
+    
 class FocusManual(VISCACommand):
+    
+    def __init__(self, cameraID = 1):
+        super(FocusManual, self).__init__(cameraID)
+        
     def getInnerBytes(self):
         return [0x01, 0x04, 0x38, 0x03]
     
@@ -77,18 +127,90 @@ class MemorySet(VISCACommand):
     def getInnerBytes(self):
         return [0x01, 0x04, 0x3F, 0x01, self.preset]
     
+    
 class MemoryRecall(VISCACommand):
     
     def __init__(self, preset, cameraID = 1):
-        super(MemorySet, self).__init__(cameraID)
+        super(MemoryRecall, self).__init__(cameraID)
         self.preset = preset
     
     def getInnerBytes(self):
         return [0x01, 0x04, 0x3F, 0x02, self.preset]
 
 # Pan-tiltDrive
-
 class MoveUp(VISCACommand):
+    
     def __init__(self, speed, cameraID = 1):
         super(MoveUp, self).__init__(cameraID)
         self.speed = speed
+        
+    def getInnerBytes(self):
+        return [0x01, 0x06, 0x01, self.speed, self.speed, 0x03, 0x01]
+
+
+class MoveDown(VISCACommand):
+    
+    def __init__(self, speed, cameraID = 1):
+        super(MoveDown, self).__init__(cameraID)
+        self.speed = speed
+        
+    def getInnerBytes(self):
+        return [0x01, 0x06, 0x01, self.speed, self.speed, 0x03, 0x02]
+
+
+class MoveLeft(VISCACommand):
+    
+    def __init__(self, speed, cameraID = 1):
+        super(MoveLeft, self).__init__(cameraID)
+        self.speed = speed
+        
+    def getInnerBytes(self):
+        return [0x01, 0x06, 0x01, self.speed, self.speed, 0x03, 0x03]
+
+
+class MoveRight(VISCACommand):
+    
+    def __init__(self, speed, cameraID = 1):
+        super(MoveRight, self).__init__(cameraID)
+        self.speed = speed
+        
+    def getInnerBytes(self):
+        return [0x01, 0x06, 0x01, self.speed, self.speed, 0x02, 0x03]
+
+
+class MoveStop(VISCACommand):
+    
+    def __init__(self, cameraID = 1):
+        super(MoveStop, self).__init__(cameraID)
+        
+    def getInnerBytes(self):
+        return [0x01, 0x06, 0x01, 0, 0, 0x03, 0x03]
+
+
+class MoveTo(VISCACommand):
+    
+    def __init__(self, speed, position, cameraID = 1):
+        super(MoveTo, self).__init__(cameraID)
+        self.speed = speed
+        self.position = position
+        
+    def getInnerBytes(self):
+        p = self.position.pan
+        t = self.position.tilt
+        return [0x01, 
+                0x06, 
+                0x02, 
+                self.speed, 
+                self.speed,
+                # Pan x 4 bytes
+                (p & 0xF000) >> 12,
+                (p & 0x0F00) >> 8,
+                (p & 0x00F0) >> 4,
+                (p & 0x000F),
+                # Tilt x 4 bytes
+                (t & 0xF000) >> 12,
+                (t & 0x0F00) >> 8,
+                (t & 0x00F0) >> 4,
+                (t & 0x000F)
+                ]
+    
