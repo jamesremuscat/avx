d51c471a577c15bbc292a93c9a3ac28eb55ea090
James Muscat <jrem@corefiling.com>
2013-03-21 17:56:29 +0000

    Start to encapsulate VISCA commands so that they can eventually be passed through a controller to more directly control cameras.

---
+++ src/org/muscat/staldates/aldatesx/devices/VISCACommands.py
@@ -0,0 +1,70 @@
+'''
+Created on 21 Mar 2013
+
+@author: jrem
+'''
+
+class VISCACommand(object):
+    '''
+    Encapsulates a VISCA command.
+    '''
+
+    def __init__(self, cameraID = 1):
+        self.cameraID = cameraID
+        
+    def getBytes(self):
+        return [0x80 + self.cameraID] + self.getInnerBytes() + [0xFF]
+    
+
+# CAM_Zoom
+class ZoomStop(VISCACommand):
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x07, 0x00]
+        
+class ZoomIn(VISCACommand):
+    
+    def __init__(self, speed = 2, cameraID = 1):
+        super(ZoomIn, self).__init__(cameraID)
+        if 1 < speed < 8:
+            self.speed = speed
+        else:
+            self.speed = 2
+    
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x07, 0x20 + self.speed]
+    
+class ZoomOut(VISCACommand):
+    
+    def __init__(self, speed = 2, cameraID = 1):
+        super(ZoomIn, self).__init__(cameraID)
+        if 1 < speed < 8:
+            self.speed = speed
+        else:
+            self.speed = 2
+    
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x07, 0x30 + self.speed]
+    
+    
+# CAM_Focus
+class FocusStop(VISCACommand):
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x08, 0x00]
+    
+class FocusFar(VISCACommand):
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x08, 0x02]
+    
+class FocusNear(VISCACommand):
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x08, 0x03]
+    
+class FocusAuto(VISCACommand):
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x38, 0x02]
+    
+class FocusManual(VISCACommand):
+    def getInnerBytes(self):
+        return [0x01, 0x04, 0x38, 0x03]
+    
+    
