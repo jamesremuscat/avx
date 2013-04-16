b4ccf6cad48178a5370c8f7c40ccfaae2a39f6b0
James Muscat <jamesremuscat@gmail.com>
2013-04-16 18:26:44 +0100

    Apparently I'm running two different versions of unittest. One of them doesn't have this method. Let's do it by hand, shall we?

--- src/org/muscat/staldates/aldatesx/ui/tests/TestVideoSwitcher.py
+++ src/org/muscat/staldates/aldatesx/ui/tests/TestVideoSwitcher.py
@@ -32,7 +32,7 @@ class TestVideoSwitcher(GuiTest):
 
     def testSendInputsToOutputs(self):
         outputsGrid = self.vs.findChild(OutputsGrid)
-        self.assertIsNotNone(outputsGrid)
+        self.assertTrue(outputsGrid != None)
 
         self.vs.btnCamera1.click()
         self.preview.sendInputToOutput.assert_called_with(1, 1)  # Camera 1 is previewed
