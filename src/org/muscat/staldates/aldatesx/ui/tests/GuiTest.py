f0609fccf330f55bb3c8bb3101d81ab63e067b0b
James Muscat <jrem@corefiling.com>
2013-04-17 18:54:20 +0100

    Some tests for the main window.

--- src/org/muscat/staldates/aldatesx/ui/tests/GuiTest.py
+++ src/org/muscat/staldates/aldatesx/ui/tests/GuiTest.py
@@ -3,7 +3,7 @@ Created on 16 Apr 2013
 
 @author: jrem
 '''
-from PySide.QtGui import QApplication
+from PySide.QtGui import QApplication, QAbstractButton
 import unittest
 
 
@@ -11,6 +11,13 @@ class GuiTest(unittest.TestCase):
 
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.app = QApplication.instance() # checks if QApplication already exists
-        if not self.app: # create QApplication if it doesn't exist
+        self.app = QApplication.instance()  # checks if QApplication already exists
+        if not self.app:  # create QApplication if it doesn't exist
             self.app = QApplication([])
+
+    def findButton(self, parent, text):
+        buttons = parent.findChildren(QAbstractButton)
+        for b in buttons:
+            if b.text() == text:
+                return b
+        return None
---
+++ src/org/muscat/staldates/aldatesx/ui/tests/TestMainWindow.py
@@ -0,0 +1,57 @@
+'''
+Created on 17 Apr 2013
+
+@author: jrem
+'''
+from org.muscat.staldates.aldatesx.ui.tests.GuiTest import GuiTest
+from org.muscat.staldates.aldatesx.Controller import Controller
+from org.muscat.staldates.aldatesx.ui.MainWindow import MainWindow
+from org.muscat.staldates.aldatesx.ui.widgets.SystemPowerWidget import SystemPowerWidget
+from mock import MagicMock
+from org.muscat.staldates.aldatesx.ui.VideoSwitcher import VideoSwitcher
+from org.muscat.staldates.aldatesx.ui.widgets.LogViewer import LogViewer
+
+
+class TestMainWindow(GuiTest):
+
+    def setUp(self):
+        GuiTest.setUp(self)
+        self.mockController = Controller()
+        self.mockController.systemPowerOn = MagicMock(return_value=1)
+        self.mockController.systemPowerOff = MagicMock(return_value=1)
+
+        self.main = MainWindow(self.mockController)
+
+    def testSystemPower(self):
+        spcButton = self.findButton(self.main, "System Power")
+        self.assertFalse(spcButton == None)
+
+        spcButton.click()
+        spc = self.main.stack.currentWidget()
+        self.assertTrue(isinstance(spc, SystemPowerWidget))
+        spc.btnOn.click()
+        self.assertEquals(self.mockController.systemPowerOn.call_count, 1)
+
+        spc.btnOff.click()
+        self.assertEquals(self.mockController.systemPowerOff.call_count, 1)
+
+        spc.b.click()
+        self.assertTrue(isinstance(self.main.stack.currentWidget(), VideoSwitcher))
+
+    def testLog(self):
+        entry = FakeLogEntry("2013-04-17 18:45:38", "ERROR", "This is a test message")
+        self.mockController.getLog = MagicMock(return_value=[entry])
+
+        logButton = self.findButton(self.main, "Log")
+        logButton.click()
+        lw = self.main.stack.currentWidget()
+        self.assertTrue(isinstance(lw, LogViewer))
+        self.assertEqual(self.mockController.getLog.call_count, 1)
+
+
+class FakeLogEntry(object):
+
+    def __init__(self, asctime, level, message):
+        self.asctime = asctime
+        self.levelname = level
+        self.message = message
