33fa4cf3f5b8dbfeb2e67eb32b73cb6df5d1d7db
James Muscat <jamesremuscat@gmail.com>
2013-06-02 12:11:25 +0100

    Debug output for Controller when dealing with screens and blinds.
    --HG--
    extra : source : 5436cdeb810565fde58f4a06a1d40cd2572ea96c

--- src/org/muscat/staldates/aldatesx/Controller.py
+++ src/org/muscat/staldates/aldatesx/Controller.py
@@ -235,6 +235,7 @@ class Controller(object):
 
     def raiseUp(self, device, number):
         if self.hasDevice(device):
+            logging.debug("Raising " + device + ":" + str(number))
             d = self.devices[device]
             d.raiseUp(number)
         else:
@@ -243,6 +244,7 @@ class Controller(object):
 
     def lower(self, device, number):
         if self.hasDevice(device):
+            logging.debug("Lowering " + device + ":" + str(number))
             d = self.devices[device]
             d.lower(number)
         else:
@@ -251,6 +253,7 @@ class Controller(object):
 
     def stop(self, device, number):
         if self.hasDevice(device):
+            logging.debug("Stopping " + device + ":" + str(number))
             d = self.devices[device]
             d.stop(number)
         else:
