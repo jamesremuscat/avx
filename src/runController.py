89f53dac57652dbb44d8ff9e0eabf20167eb441f
James Muscat <jamesremuscat@gmail.com>
2014-03-23 14:53:29 +0000

    I think we're mature enough now to default to INFO-level logging.

--- src/runController.py
+++ src/runController.py
@@ -3,7 +3,7 @@ from org.muscat.staldates.aldatesx.controller.Controller import Controller
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
     controller = Controller()
     controller.loadConfig("config.json")
     controller.initialise()
