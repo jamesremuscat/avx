b914cde766d342e6c4f336e493dba16a22ed73e5
James Muscat <jamesremuscat@gmail.com>
2014-03-23 13:38:56 +0000

    Ze buttons... zey do nothing! (Yet. Because I don't know what they're supposed to do. Yet.)

--- src/org/muscat/staldates/aldatesx/ui/widgets/LightingControl.py
+++ src/org/muscat/staldates/aldatesx/ui/widgets/LightingControl.py
@@ -1,4 +1,6 @@
 from org.muscat.staldates.aldatesx.ui.widgets.Screens import ScreenWithBackButton
+from PySide.QtGui import QHBoxLayout, QVBoxLayout
+from org.muscat.staldates.aldatesx.ui.widgets.Buttons import ExpandingButton
 
 
 class LightingControl(ScreenWithBackButton):
@@ -9,3 +11,36 @@ class LightingControl(ScreenWithBackButton):
     def __init__(self, controller, mainWindow):
         self.controller = controller
         ScreenWithBackButton.__init__(self, "Welcome Area Lights", mainWindow)
+
+    def makeContent(self):
+        layout = QHBoxLayout()
+
+        gallery = QVBoxLayout()
+
+        galleryOn = ExpandingButton()
+        galleryOn.setText("Gallery")
+        gallery.addWidget(galleryOn)
+
+        external = ExpandingButton()
+        external.setText("External Lights")
+        gallery.addWidget(external)
+
+        layout.addLayout(gallery)
+
+        welcomeArea = QVBoxLayout()
+
+        full = ExpandingButton()
+        full.setText("100%")
+        welcomeArea.addWidget(full)
+
+        std = ExpandingButton()
+        std.setText("70%")
+        welcomeArea.addWidget(std)
+
+        off = ExpandingButton()
+        off.setText("Off")
+        welcomeArea.addWidget(off)
+
+        layout.addLayout(welcomeArea)
+
+        return layout
