3caff32a1b01e513b6b37c39afea778d25895009
James Muscat <jamesremuscat@gmail.com>
2014-03-25 21:30:54 +0000

    Allow the -c option for cli.py (works with python -i).

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,25 @@
 import Pyro4
 from org.muscat.staldates.aldatesx.controller.Controller import *
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
-    c = Pyro4.Proxy("PYRONAME:" + Controller.pyroName)
+    parser = ArgumentParser()
+    parser.add_argument("-c",
+                        help="Specify the controller ID to connect to",
+                        metavar="CONTROLLERID",
+                        default="")
+    args = parser.parse_args()
+
+    if args.c != "":
+        controllerAddress = "PYRONAME:" + Controller.pyroName + "." + args.c
+    else:
+        controllerAddress = "PYRONAME:" + Controller.pyroName
+
+    logging.info("Connecting to controller at " + controllerAddress)
+
+    c = Pyro4.Proxy(controllerAddress)
+    remoteVersion = c.getVersion()
+
+    if remoteVersion != Controller.version:
+        raise VersionMismatchError(remoteVersion, Controller.version)
     print "Controller c available."
