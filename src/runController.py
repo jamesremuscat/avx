f39c8c0be0293e93272f6251f4a4a285779f3887
James Muscat <jamesremuscat@gmail.com>
2014-03-25 22:50:55 +0000

    How did I survive this long without a debug commandline option?

--- src/runController.py
+++ src/runController.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
+from argparse import ArgumentParser
 from org.muscat.staldates.aldatesx.controller.Controller import Controller
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--debug",
+                        help="Show debugging output.",
+                        action="store_true")
+    args = parser.parse_args()
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=(logging.DEBUG if args.debug else logging.INFO))
     controller = Controller()
     controller.loadConfig("config.json")
     controller.initialise()
