1f8a7bb226656fddd6fbc1f5e744a5bc56097422
James Muscat <jamesremuscat@gmail.com>
2014-04-03 21:48:01 +0100

    Explicitly cast arguments to int so they can be used via HTTP.
    That's the drawback of this method, I guess...

--- src/org/muscat/avx/controller/amBxController.py
+++ src/org/muscat/avx/controller/amBxController.py
@@ -2,7 +2,7 @@ class amBxController(object):
 
     def setColour(self, device, light, red, green, blue):
         def reallySetColour():
-            return self.devices[device].setColour(light, red, green, blue)
+            return self.devices[device].setColour(int(light), int(red), int(green), int(blue))
         return self.withDevice(device, reallySetColour)
 
     def allOff(self, device):
