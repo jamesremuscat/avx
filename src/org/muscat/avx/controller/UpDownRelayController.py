837fa7bf3cbd89d13c2997f17b3176670f18d59a
James Muscat <jamesremuscat@gmail.com>
2014-05-06 21:09:23 +0100

    Use new decorator for UpDownRelayController methods.

--- src/org/muscat/avx/controller/UpDownRelayController.py
+++ src/org/muscat/avx/controller/UpDownRelayController.py
@@ -1,24 +1,23 @@
+from org.muscat.avx.controller.ControllerHelpers import deviceMethod
 import logging
 
 
 class UpDownRelayController:
+
+    @deviceMethod
     def raiseUp(self, device, number):
-        def reallyRaise():
-            logging.debug("Raising " + device + ":" + str(number))
-            d = self.devices[device]
-            d.raiseUp(number)
-        return self.withDevice(device, reallyRaise)
+        logging.debug("Raising " + device + ":" + str(number))
+        d = self.devices[device]
+        return d.raiseUp(number)
 
+    @deviceMethod
     def lower(self, device, number):
-        def reallyLower():
-            logging.debug("Lowering " + device + ":" + str(number))
-            d = self.devices[device]
-            d.lower(number)
-        return self.withDevice(device, reallyLower)
+        logging.debug("Lowering " + device + ":" + str(number))
+        d = self.devices[device]
+        return d.lower(number)
 
+    @deviceMethod
     def stop(self, device, number):
-        def reallyStop():
-            logging.debug("Stopping " + device + ":" + str(number))
-            d = self.devices[device]
-            d.stop(number)
-        return self.withDevice(device, reallyStop)
+        logging.debug("Stopping " + device + ":" + str(number))
+        d = self.devices[device]
+        return d.stop(number)
