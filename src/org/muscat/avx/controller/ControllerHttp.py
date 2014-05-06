48b1a140574eaa053539a4adca1628006c01419e
James Muscat <jamesremuscat@gmail.com>
2014-05-06 21:53:46 +0100

    Add a modicum of security, require a special decorator for HTTP access.
    Functions that don't have the @httpAccessible decorator won't be
    available over the HTTP controller interface.

--- src/org/muscat/avx/controller/ControllerHttp.py
+++ src/org/muscat/avx/controller/ControllerHttp.py
@@ -3,6 +3,9 @@ from threading import Thread
 import logging
 
 
+httpAccessibleKey = "isHTTPAccessible"
+
+
 class ControllerHttp(Thread):
     '''
     An HTTP interface to a Controller's methods.
@@ -19,26 +22,43 @@ class ControllerHttp(Thread):
         def do_GET(self):
             parts = self.path.split('/')
             if hasattr(self.controller, parts[1]):
-                attr = getattr(self.controller, parts[1])
-                args = parts[2].split(',')
-                if (self.controller.hasDevice(args[0])):
-                    result = attr(*args)
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.wfile.write("OK " + str(result))
+                function = getattr(self.controller, parts[1])
+                if (function.func_dict.get(httpAccessibleKey)):
+                    args = parts[2].split(',')
+                    if (self.controller.hasDevice(args[0])):
+                        result = function(*args)
+                        self.send_response(200)
+                        self.send_header('Content-type', 'text/plain')
+                        self.end_headers()
+                        self.wfile.write("OK " + str(result))
+                    else:
+                        self.send_response(404)
+                        self.send_header('Content-type', 'text/plain')
+                        self.end_headers()
+                        self.wfile.write("No such device: " + args[0])
                 else:
-                    self.send_response(404)
+                    self.send_response(403)
                     self.send_header('Content-type', 'text/plain')
                     self.end_headers()
-                    self.wfile.write("No such device: " + args[0])
+                    self.wfile.write("Not permitted to call " + parts[1] + " over HTTP (missing @httpAccessible decorator?")
             else:
                 self.send_response(400)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
-                self.wfile.write("No such method: " + parts[1])
+                self.wfile.write("No such function: " + parts[1])
 
     def run(self):
         self.server = HTTPServer(("", self.port), self.ControllerHttpRequestHandler)
         logging.info("Started HTTP server on port " + str(self.port))
         self.server.serve_forever()
+
+
+def httpAccessible(originalFunction):
+    '''
+    Decorator function that marks the given function as accessible over HTTP.
+
+    The ControllerHTTPRequestHandler will refuse to call any function on the
+    controller that does NOT have this decorator.
+    '''
+    originalFunction.func_dict[httpAccessibleKey] = True
+    return originalFunction
