11bdee90628bf8bda42927fa4719a0672785a033
James Muscat <jamesremuscat@gmail.com>
2014-05-06 21:07:57 +0100

    Use the new decorator for VISCAController methods.

--- src/org/muscat/avx/controller/VISCAController.py
+++ src/org/muscat/avx/controller/VISCAController.py
@@ -1,114 +1,105 @@
+from org.muscat.avx.controller.ControllerHelpers import deviceMethod
 import logging
 
 
 class VISCAController:
+
+    @deviceMethod
     def move(self, camDeviceID, direction):
-        def reallyMove():
-            camera = self.devices[camDeviceID]
-            if direction == CameraMove.Left:
-                return camera.moveLeft()
-            elif direction == CameraMove.Right:
-                return camera.moveRight()
-            elif direction == CameraMove.Up:
-                return camera.moveUp()
-            elif direction == CameraMove.Down:
-                return camera.moveDown()
-            elif direction == CameraMove.Stop:
-                return camera.stop()
-        return self.withDevice(camDeviceID, reallyMove)
+        camera = self.devices[camDeviceID]
+        if direction == CameraMove.Left:
+            return camera.moveLeft()
+        elif direction == CameraMove.Right:
+            return camera.moveRight()
+        elif direction == CameraMove.Up:
+            return camera.moveUp()
+        elif direction == CameraMove.Down:
+            return camera.moveDown()
+        elif direction == CameraMove.Stop:
+            return camera.stop()
 
+    @deviceMethod
     def zoom(self, camDeviceID, zoomType):
-        def reallyZoom():
-            camera = self.devices[camDeviceID]
-            if zoomType == CameraZoom.Tele:
-                return camera.zoomIn()
-            elif zoomType == CameraZoom.Wide:
-                return camera.zoomOut()
-            elif zoomType == CameraZoom.Stop:
-                return camera.zoomStop()
-        return self.withDevice(camDeviceID, reallyZoom)
+        camera = self.devices[camDeviceID]
+        if zoomType == CameraZoom.Tele:
+            return camera.zoomIn()
+        elif zoomType == CameraZoom.Wide:
+            return camera.zoomOut()
+        elif zoomType == CameraZoom.Stop:
+            return camera.zoomStop()
 
+    @deviceMethod
     def focus(self, camDeviceID, focusType):
-        def reallyFocus():
-            camera = self.devices[camDeviceID]
-            if focusType == CameraFocus.Auto:
-                return camera.focusAuto()
-            elif focusType == CameraFocus.Near:
-                return camera.focusNear()
-            elif focusType == CameraFocus.Far:
-                return camera.focusFar()
-            elif focusType == CameraFocus.Stop:
-                return camera.focusStop()
-        return self.withDevice(camDeviceID, reallyFocus)
+        camera = self.devices[camDeviceID]
+        if focusType == CameraFocus.Auto:
+            return camera.focusAuto()
+        elif focusType == CameraFocus.Near:
+            return camera.focusNear()
+        elif focusType == CameraFocus.Far:
+            return camera.focusFar()
+        elif focusType == CameraFocus.Stop:
+            return camera.focusStop()
 
+    @deviceMethod
     def exposure(self, camDeviceID, exposureType):
-        def reallyExpose():
-            camera = self.devices[camDeviceID]
-            if exposureType == CameraExposure.Brighter:
-                return camera.brighter()
-            elif exposureType == CameraExposure.Darker:
-                return camera.darker()
-            elif exposureType == CameraExposure.Auto:
-                return camera.autoExposure()
-        return self.withDevice(camDeviceID, reallyExpose)
+        camera = self.devices[camDeviceID]
+        if exposureType == CameraExposure.Brighter:
+            return camera.brighter()
+        elif exposureType == CameraExposure.Darker:
+            return camera.darker()
+        elif exposureType == CameraExposure.Auto:
+            return camera.autoExposure()
 
+    @deviceMethod
     def backlightComp(self, camDeviceID, compensation):
-        def reallyBacklight():
             camera = self.devices[camDeviceID]
             if compensation:
                 return camera.backlightCompOn()
             else:
                 return camera.backlightCompOff()
-        return self.withDevice(camDeviceID, reallyBacklight)
 
+    @deviceMethod
     def savePreset(self, camDeviceID, preset):
-        def reallySave():
-            camera = self.devices[camDeviceID]
-            logging.debug("Saving preset " + str(preset) + " on device " + camDeviceID)
-            camera.storePreset(preset)
-        return self.withDevice(camDeviceID, reallySave)
+        camera = self.devices[camDeviceID]
+        logging.debug("Saving preset " + str(preset) + " on device " + camDeviceID)
+        camera.storePreset(preset)
 
+    @deviceMethod
     def recallPreset(self, camDeviceID, preset):
-        def reallyRecall():
-            camera = self.devices[camDeviceID]
-            logging.debug("Recalling preset " + str(preset) + " on device " + camDeviceID)
-            camera.recallPreset(preset)
-        return self.withDevice(camDeviceID, reallyRecall)
+        camera = self.devices[camDeviceID]
+        logging.debug("Recalling preset " + str(preset) + " on device " + camDeviceID)
+        return camera.recallPreset(preset)
 
+    @deviceMethod
     def whiteBalance(self, camDeviceID, wbSetting):
-        def reallyBalance():
-            camera = self.devices[camDeviceID]
-            if wbSetting == CameraWhiteBalance.Auto:
-                return camera.whiteBalanceAuto()
-            elif wbSetting == CameraWhiteBalance.Indoor:
-                return camera.whiteBalanceIndoor()
-            elif wbSetting == CameraWhiteBalance.Outdoor:
-                return camera.whiteBalanceOutdoor()
-            elif wbSetting == CameraWhiteBalance.OnePush:
-                return camera.whiteBalanceOnePush()
-            elif wbSetting == CameraWhiteBalance.Trigger:
-                return camera.whiteBalanceOnePushTrigger()
-        return self.withDevice(camDeviceID, reallyBalance)
+        camera = self.devices[camDeviceID]
+        if wbSetting == CameraWhiteBalance.Auto:
+            return camera.whiteBalanceAuto()
+        elif wbSetting == CameraWhiteBalance.Indoor:
+            return camera.whiteBalanceIndoor()
+        elif wbSetting == CameraWhiteBalance.Outdoor:
+            return camera.whiteBalanceOutdoor()
+        elif wbSetting == CameraWhiteBalance.OnePush:
+            return camera.whiteBalanceOnePush()
+        elif wbSetting == CameraWhiteBalance.Trigger:
+            return camera.whiteBalanceOnePushTrigger()
 
+    @deviceMethod
     def getPosition(self, camDeviceID):
-        def reallyGetPos():
-            camera = self.devices[camDeviceID]
-            logging.debug("Querying position of device " + camDeviceID)
-            return camera.getPosition()
-        return self.withDevice(camDeviceID, reallyGetPos)
+        camera = self.devices[camDeviceID]
+        logging.debug("Querying position of device " + camDeviceID)
+        return camera.getPosition()
 
+    @deviceMethod
     def goto(self, camDeviceID, pos, panSpeed, tiltSpeed):
-        def reallyGoto():
-            camera = self.devices[camDeviceID]
-            logging.debug("Setting position of device " + camDeviceID)
-            return camera.goto(pos, panSpeed, tiltSpeed)
-        return self.withDevice(camDeviceID, reallyGoto)
+        camera = self.devices[camDeviceID]
+        logging.debug("Setting position of device " + camDeviceID)
+        return camera.goto(pos, panSpeed, tiltSpeed)
 
+    @deviceMethod
     def cameraCommand(self, camDeviceID, command):
-        def reallyCommand():
-            camera = self.devices[camDeviceID]
-            return camera.execute(command)
-        return self.withDevice(camDeviceID, reallyCommand)
+        camera = self.devices[camDeviceID]
+        return camera.execute(command)
 
 
 class CameraMove():
