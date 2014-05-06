b13361c63e7dc41f34bd4e78f28f8863c5bc50a2
James Muscat <jamesremuscat@gmail.com>
2014-05-06 21:12:36 +0100

    Use new decorator for scan converter methods.

--- src/org/muscat/avx/controller/ScanConverterController.py
+++ src/org/muscat/avx/controller/ScanConverterController.py
@@ -1,44 +1,41 @@
+from org.muscat.avx.controller.ControllerHelpers import deviceMethod
 
 
 class ScanConverterController:
+
+    @deviceMethod
     def toggleOverscan(self, scDevice, overscan):
-        def reallyOverscan():
-            sc = self.devices[scDevice]
-            if overscan:
-                return sc.overscanOn()
-            else:
-                return sc.overscanOff()
-        return self.withDevice(scDevice, reallyOverscan)
+        sc = self.devices[scDevice]
+        if overscan:
+            return sc.overscanOn()
+        else:
+            return sc.overscanOff()
 
+    @deviceMethod
     def toggleFreeze(self, scDevice, freeze):
-        def reallyFreeze():
-            sc = self.devices[scDevice]
-            if freeze:
-                return sc.freeze()
-            else:
-                return sc.unfreeze()
-        return self.withDevice(scDevice, reallyFreeze)
+        sc = self.devices[scDevice]
+        if freeze:
+            return sc.freeze()
+        else:
+            return sc.unfreeze()
 
+    @deviceMethod
     def toggleOverlay(self, scDevice, overlay):
-        def reallyOverlay():
-            sc = self.devices[scDevice]
-            if overlay:
-                return sc.overlayOn()
-            else:
-                return sc.overlayOff()
-        return self.withDevice(scDevice, reallyOverlay)
+        sc = self.devices[scDevice]
+        if overlay:
+            return sc.overlayOn()
+        else:
+            return sc.overlayOff()
 
+    @deviceMethod
     def toggleFade(self, scDevice, fade):
-        def reallyFade():
-            sc = self.devices[scDevice]
-            if fade:
-                return sc.fadeOut()
-            else:
-                return sc.fadeIn()
-        return self.withDevice(scDevice, reallyFade)
+        sc = self.devices[scDevice]
+        if fade:
+            return sc.fadeOut()
+        else:
+            return sc.fadeIn()
 
+    @deviceMethod
     def recalibrate(self, scDevice):
-        def reallyRecalibrate():
-            sc = self.devices[scDevice]
-            return sc.recalibrate()
-        return self.withDevice(scDevice, reallyRecalibrate)
+        sc = self.devices[scDevice]
+        return sc.recalibrate()
