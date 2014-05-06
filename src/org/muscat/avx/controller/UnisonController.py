04aa8b58952f333df008b3e8cc3b00c73e74193a
James Muscat <jamesremuscat@gmail.com>
2014-05-06 21:10:08 +0100

    Use new decorator for UnisonController methods.

--- src/org/muscat/avx/controller/UnisonController.py
+++ src/org/muscat/avx/controller/UnisonController.py
@@ -1,17 +1,17 @@
+from org.muscat.avx.controller.ControllerHelpers import deviceMethod
 import logging
 
 
 class UnisonController:
-    def activate(self, device, objectID):
-        def reallyActivate():
-            logging.debug("Activating preset " + objectID + " on " + device)
-            d = self.devices[device]
-            return d.activate(objectID)
-        return self.withDevice(device, reallyActivate)
 
+    @deviceMethod
+    def activate(self, device, objectID):
+        logging.debug("Activating preset " + objectID + " on " + device)
+        d = self.devices[device]
+        return d.activate(objectID)
+
+    @deviceMethod
     def deactivate(self, device, objectID):
-        def reallyDeactivate():
-            logging.debug("Deactivating preset " + objectID + " on " + device)
-            d = self.devices[device]
-            return d.deactivate(objectID)
-        return self.withDevice(device, reallyDeactivate)
+        logging.debug("Deactivating preset " + objectID + " on " + device)
+        d = self.devices[device]
+        return d.deactivate(objectID)
