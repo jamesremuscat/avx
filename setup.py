18c4868104f0d1baca9a16c3030c856202115a88
James Muscat <jamesremuscat@gmail.com>
2014-06-23 23:46:24 +0100

    Update install dependencies (Pyro4 >= 4.20) and bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='avx',
-    version='0.92',
+    version='0.93',
     description='Library for controlling A/V devices',
     author='James Muscat',
     author_email='jamesremuscat@gmail.com',
@@ -12,7 +12,7 @@ setup(
       long_description="""\
       AVX is a library for controlling A/V devices such as video switchers. ...
       """,
-    install_requires=["Pyro4", "pyserial"],
+    install_requires=["Pyro4 >= 4.20", "pyserial"],
     entry_points={
         'console_scripts': [
             'avx-controller = org.muscat.avx.controller.Controller:main',
