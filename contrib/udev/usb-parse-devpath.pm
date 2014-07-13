a245b1bebb69b74a96aa7b16ff8c6ec52363c9b4
James Muscat <jamesremuscat@gmail.com>
2014-07-13 22:57:40 +0100

    Make the usb-parse-devpath script more clever: it looks for something in the right format, rather than a fixed position in the path.
    Closes #21. This change will require the config.json script to be
    updated for any installations depending on the old behaviour.

--- contrib/udev/usb-parse-devpath.pm
+++ contrib/udev/usb-parse-devpath.pm
@@ -1,9 +1,17 @@
 #!/usr/bin/perl -w
 
+# A device path looks something like
+# /devices/platform/bcm2708_usb/usb1/1-1/1-1.2/1-1.2:1.0/ttyUSB0/tty/ttyUSB0
+
 @items = split("/", $ARGV[0]);
-for ($i = 0; $i < @items; $i++) {
-    if ($items[$i] =~ m/^usb[0-9]+$/) {
-        print $items[$i + 4] . "\n";
+
+# Walk backwards along the path until we find a device:configuration.interface
+# then print it out and exit
+
+for ($i = @items - 1; $i > 0; $i--) {
+    if ($items[$i] =~ m/^[0-9]-([0-9]+\.)+[0-9]+:[0-9]+\.[0-9]+$/) {
+        print $items[$i] . "\n";
         last;
     }
 }
+
