a536e72f2b9c8b6f5622e1aaea7e8cf6b9000971
James Muscat <jamesremuscat@gmail.com>
2015-12-06 12:31:59 +0000

    pep8 wants an extra blank line.

--- src/avx/PyroUtils.py
+++ src/avx/PyroUtils.py
@@ -8,5 +8,6 @@ def setHostname():
         logging.info("Using " + ip + " as hostname")
         Pyro4.config.HOST = ip
 
+
 def getHostname():
     return subprocess.check_output(["hostname", "-I"]).split()[0].rstrip()
