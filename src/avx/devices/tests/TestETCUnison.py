7ea681b3ba6d80cf14f89ad17bb70fa9b19036da
James Muscat <jamesremuscat@gmail.com>
2016-11-03 17:13:01 +0000

    Tidy up unused variable.

--- src/avx/devices/tests/TestETCUnison.py
+++ src/avx/devices/tests/TestETCUnison.py
@@ -16,8 +16,7 @@ class TestETCUnison(unittest.TestCase):
         self.assertEqual('ee1700004042616c6c726f6f6d2e44696e6e65722e414354490000', cmd.getByteString().encode('hex_codec'))
 
         try:
-            cmd2 = UnisonCommand("Very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long string")
-            cmd2.__class__  # never executed, only here to prevent unused variable warning
+            _ = UnisonCommand("Very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long string")
             self.fail("Should have balked at overly lengthy command string")
         except (CommandStringTooLongError):
             pass
