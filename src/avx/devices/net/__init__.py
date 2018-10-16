4c4e06e6eb3edf82729f93779477c0be036fdc99
James Muscat <jamesremuscat@gmail.com>
2018-10-16 14:14:15 +0100

    Remove debug spam.

--- src/avx/devices/net/__init__.py
+++ src/avx/devices/net/__init__.py
@@ -58,7 +58,6 @@ class TCPDevice(Device):
 
     def send(self, data):
         if self.socket:
-            print "Sending..."
             self.socket.send(data)
         else:
             self.log.warn("Tried to send data without a socket: {}".format(data))
