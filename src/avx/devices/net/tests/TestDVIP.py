96cecffad7ef5b2d37fc566726a5aebfd774e8f0
James Muscat <jamesremuscat@gmail.com>
2018-10-22 13:12:43 +0100

    Test for packet splitting.

--- src/avx/devices/net/tests/TestDVIP.py
+++ src/avx/devices/net/tests/TestDVIP.py
@@ -3,7 +3,7 @@ Created on 21 Mar 2018
 
 @author: james
 '''
-from avx.devices.net.dvip import DVIPCamera
+from avx.devices.net.dvip import DVIPCamera, _split_response
 from avx.devices.serial.SerialDevice import SerialDevice
 from mock import MagicMock
 
@@ -35,3 +35,20 @@ class TestDVIPCamera(unittest.TestCase):
         run(self.dvip.stop, [0x00, 0x0B, 0x81, 0x01, 0x06, 0x01, 0x06, 0x06, 0x03, 0x03, 0xFF])
         run(self.dvip.zoomOut, [0x00, 0x08, 0x81, 0x01, 0x04, 0x07, 0x36, 0xFF])
         run(lambda: self.dvip.storePreset(3), [0x00, 0x09, 0x81, 0x01, 0x04, 0x3F, 0x01, 0x03, 0xFF])
+
+
+class TestSplitReceivedPackets(unittest.TestCase):
+    def testSplitting(self):
+        recvd = '\x00\x00\x01\x02\x03\x04\xFF\x05\x06\xFF\x07\x08\x09\xFF'
+        split = _split_response(recvd)
+
+        self.assertEqual(len(split), 3)
+
+        self.assertEqual(
+            split,
+            [
+                [1, 2, 3, 4, 0xFF],
+                [5, 6, 0xFF],
+                [7, 8, 9, 0xFF]
+            ]
+        )
