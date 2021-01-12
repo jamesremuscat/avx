f29bd01aa89ce7681f7bea6f906c2631fb43ad1a
James Muscat <jamesremuscat@gmail.com>
2021-01-12 16:06:33 +0000

    Migrate from pytest-runner to tox.

--- setup.py
+++ setup.py
@@ -21,16 +21,15 @@ setup(
     author_email='jamesremuscat@gmail.com',
     url='https://github.com/jamesremuscat/avx',
     packages=find_packages('src', exclude=["*.tests"]),
-    package_dir = {'':'src'},
-      long_description="""\
-      AVX is a library for controlling A/V devices such as video switchers. ...
-      """,
-    setup_requires=["pytest-runner", "pytest-cov"],
-    tests_require=["mock", "pytest"],
+    package_dir={'': 'src'},
+    long_description="""\
+    AVX is a library for controlling A/V devices such as video switchers. ...
+    """,
+    tests_require=["mock", "pytest", "pytest-cov"],
     install_requires=["enum34", "Pyro4>=4.20,!=4.45", "pyserial", "pyusb", "semantic_version"],
     entry_points={
         'console_scripts': [
             'avx-controller = avx.controller.Controller:main',
-            ],
-        }
-      )
+        ],
+    }
+)
